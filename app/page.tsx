4a9387cfc3900a7f5c76363f51ef82b4d7d93c8f
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-07-29 07:16:46 +0545

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,122 @@
-import Image from "next/image";
+'use client'
+import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+import { clusterApiUrl, Connection, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { useState } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
+
+
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [receiver, setReceiver]: any = useState("null")
+  const [solAmt, setSolAmt]: any = useState(0)
+  const [loading, setLoading] = useState(false)
+
+  const { publicKey, sendTransaction, wallet } = useWallet()
+
+
+  const lamports = 1000000000 * solAmt;
+
+  const connection = new Connection(clusterApiUrl('devnet'), ('confirmed'))
+
+
+
+
+
+  // const sendSolana = useCallback(async () => {
+  //   if (!publicKey) {
+  //     alert("Connect your wallet first");
+  //     return;
+  //   }
+
+  //   try {
+  //     const toPubkey = new PublicKey('CULHUoFYC7FXcD6x1i2UvfFaCha64F1t3iuyo9ijVpr');
+  //     const { blockhash } = await connection.getLatestBlockhash('confirmed');
+
+  //     const transaction = new Transaction({
+  //       recentBlockhash: blockhash,
+  //       feePayer: publicKey,
+  //     }).add(
+  //       SystemProgram.transfer({
+  //         fromPubkey: publicKey,
+  //         toPubkey,
+  //         lamports,
+  //       })
+  //     );
+
+  //     // Let Phantom or connected wallet sign and send
+  //     const signature = await sendTransaction(transaction, connection);
+
+  //     // Wait for confirmation
+  //     await connection.confirmTransaction(signature, 'confirmed');
+  //     console.log("Transaction Signature:", signature);
+  //     alert("Transaction successful!\nSignature: " + signature);
+  //   } catch (error) {
+  //     console.error("Transaction failed:", error);
+  //     alert("Transaction failed. Check console.");
+  //   }
+  // }, [publicKey, sendTransaction, connection]);
+
+  const sendSolana = async () => {
+    setLoading(true)
+    if (!publicKey) {
+      alert("connect your wallet")
+      setLoading(false)
+      return null;
+    }
+
+
+    console.log(receiver)
+    console.log(lamports)
+    const toPubkey = new PublicKey(receiver);
+
+
+
+    console.log(publicKey, toPubkey, lamports)
+
+
+
+    const transferTransaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey,
+        lamports
+      })
+    )
+
+    const signature = await sendTransaction(transferTransaction, connection)
+    await connection.confirmTransaction(signature, 'confirmed')
+
+    setLoading(false)
+    console.log('transaction sucessfull', signature)
+    alert('transaction sucessfull')
+  }
+
+  return (
+    <>
+      <div>
+
+        <div className=' container mx-auto p-4 flex justify-between' >
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+
+
+        <section>
+          <div className=' '>
+            <input onChange={(e) => setReceiver(e.target.value)} className='border w-96 border-zinc-500/50 rounded-md px-2 py-2 m-1 outline-none ' name="" id="" placeholder='Receiver Public Key' type="text" /><br />
+            <br />
+            <input onChange={(e) => setSolAmt(e.target.value)} className='border w-96 border-zinc-500/50 rounded-md px-2 py-2 m-1 outline-none ' name="" id="" placeholder='SOL Amount' type="number" /><br />
+            <br />
+            <button onClick={sendSolana} className='bg-sky-500 px-2 py-1 rounded-md text-white w-fit border border-zinc-700 mx-auto'>{loading ? ("sending... ") : ("send solana ")} </button>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
